f130e7ba3ae598be52755def8e9c8ae8126a2c4d
LouisJustinTALLOT <lj.tallot@gmail.com>
2021-02-03 21:16:17 +0100

    différents types de déclaration

--- fortran90_basics/print_constants.f90
+++ fortran90_basics/print_constants.f90
@@ -2,6 +2,20 @@ program print_constants
     implicit none
     ! gfortran print_constants.f90 -o print_constants
 
+    !  integer :
+    integer :: un_entier
+    ! real decimal : 
+    real :: un_reel_decimal
+    ! real exponential : 
+    real :: un_reel_exponentiel
+    ! boolean : 
+    logical :: un_logical
+    ! complex
+    complex :: un_complexe
+    ! string : 
+    character :: une_lettre ! seulement une lettre
+    character(len=20) :: une_string_m20
+
 
     
 end program print_constants
