fc097d76b10adfe08667a0269605b7cabd0ad698
LouisJustinTALLOT <lj.tallot@gmail.com>
2021-02-03 21:16:40 +0100

    les paramètres sont des constantes

--- fortran90_basics/print_constants.f90
+++ fortran90_basics/print_constants.f90
@@ -16,6 +16,16 @@ program print_constants
     character :: une_lettre ! seulement une lettre
     character(len=20) :: une_string_m20
 
+    ! les paramètres : ne peuvent PAS être modifiés
+    real, parameter :: pi = 3.1415926
+    logical, parameter :: true = .true., false = .false.
+
+    ! avec les strings :
+    CHARACTER(LEN=3), PARAMETER :: YES = "yes" ! Len = 3 y
+    CHARACTER(LEN=2), PARAMETER :: NO = "no" ! Len = 2
+    CHARACTER(LEN=*), PARAMETER :: &
+        PROMPT = "What do you want?" ! Len = 17
+
 
     
 end program print_constants
