0c580f90a7bc8a6f1d5e36c918978748a5a8475b
LouisJustinTALLOT <lj.tallot@gmail.com>
2021-02-06 22:04:32 +0100

    calcul de pi, rapide même aec 100 millions d'itérations

--- Maths/approx_pi.f90
+++ Maths/approx_pi.f90
@@ -1,7 +1,23 @@
 program approx_pi
     implicit none
     
+    ! gfortran approx_pi.f90 -o approx_pi
+
     real :: pi
+    integer :: i, signe=1
+    pi = 0
+
+    do i=1,100000001, 2
+
+        ! write(*,*) i
+        pi = pi + signe/real(i)
+        ! write(*,*) pi, signe, signe/i
+        signe = signe* (-1)
+
+    end do
+    pi =  pi * 4
+
+    write(*,*) pi
 
 
 end program approx_pi
